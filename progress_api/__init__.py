6e2f0006a1bb398289bab36ea14531df9b7bcaab
Michael Ekstrand <md@ekstrandom.net>
2023-11-22 19:48:23 -0500

    pre-release version

--- progress_api/__init__.py
+++ progress_api/__init__.py
@@ -5,4 +5,4 @@ Backend-agnostic API for reporting progress.
 from .api import Progress, make_progress  # noqa: F401
 from .config import set_backend  # noqa: F401
 
-__version__ = "0.1.0"
+__version__ = "0.1.0a1"
