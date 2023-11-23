99939c8ddeb4838d8eb1642267985d7672b1cc67
Michael Ekstrand <md@ekstrandom.net>
2023-11-22 20:37:39 -0500

    use setuptools versioning

--- progress_api/__init__.py
+++ progress_api/__init__.py
@@ -2,7 +2,14 @@
 Backend-agnostic API for reporting progress.
 """
 
+from importlib.metadata import version, PackageNotFoundError
+
 from .api import Progress, make_progress  # noqa: F401
 from .config import set_backend  # noqa: F401
 
-__version__ = "0.1.0a1"
+
+try:
+    __version__ = version("progress-api")
+except PackageNotFoundError:
+    # package is not installed
+    pass
