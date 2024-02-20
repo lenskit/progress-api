0c4c23c2197201f66506f66338c50f70408e4962
Michael Ekstrand <md@ekstrandom.net>
2024-02-20 10:10:31 -0500

    fix import errors

--- progress_api/__init__.py
+++ progress_api/__init__.py
@@ -9,8 +9,10 @@ Backend-agnostic API for reporting progress.
 
 from importlib.metadata import PackageNotFoundError, version
 
-from .api import Progress, make_progress  # noqa: F401
-from .config import set_backend  # noqa: F401
+from .api import Progress, make_progress
+from .config import set_backend
+
+__all__ = ["Progress", "make_progress", "set_backend"]
 
 try:
     __version__ = version("progress-api")
